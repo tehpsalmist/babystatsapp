e82be261e292d7cd29b792e85ca1521833a4766d
Ben Steward <ronyfan102@gmail.com>
2017-07-23 15:58:06 -0400

    UI changes
    Clear fields after posting, explanatory alerts and text added for user clarity, minor formatting changes, added pink/blue background to posted guesses <li>s.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ var Model = ko.observableArray([{
 	'oz': '13',
 	'length': '22',
 	'day': '2017-07-26',
-	'time': 'Noon'
+	'time': 'Noon',
+	'genderVal': true
 }]);
 
 var newGuess = ko.observable({});
@@ -27,14 +28,31 @@ var ViewModel = function() {
 	this.length = ko.observable('');
 	this.day = ko.observable('');
 	this.time = ko.observable('');
+	this.genderVal = ko.observable(true);
 	//this.showHidePassword = ko.observable('password');
 	this.showHideGuess = ko.observable(true);
 	this.guessObject = {};
 	this.alertNoInput = ko.observable(true);
+	this.alertSuccess = ko.observable(true);
+	this.alertNewEntry = ko.observable(true);
+	this.clearFields = function() {
+		self.guesserName('');
+		//self.password('');
+		self.firstName('');
+		self.middleName('');
+		self.gender('');
+		self.lbs('');
+		self.oz('');
+		self.length('');
+		self.day('');
+		self.time('');
+		self.alertNewEntry(false);
+	};
 
 	this.submitGuess = function() {
 		self.alertNoInput(true);
 		if (self.guesserName() !== '' && self.firstName() !== '' && self.middleName() !== '' && self.lbs() !== '' && self.oz() !== '' && self.length() !== '' && self.day() !== '') {
+			self.alertSuccess(false);
 			if (self.showHideGuess() === true) {
 				self.showHideGuess(false);
 			};
@@ -52,6 +70,11 @@ var ViewModel = function() {
 	};*/
 
 	this.shareGuessToWorld = function() {
+		if (self.gender() !== 'Boy') {
+			self.genderVal(false);
+		} else {
+			self.genderVal(true);
+		};
 		self.guessObject = {
 			'guesserName': self.guesserName(),
 			//'password': self.password(),
@@ -62,11 +85,14 @@ var ViewModel = function() {
 			'oz': self.oz(),
 			'length': self.length(),
 			'day': self.day(),
-			'time': self.time()
+			'time': self.time(),
+			'genderVal': self.genderVal()
 		};
 		Model().push(self.guessObject);
 		self.modelBindings(Model());
 		self.showHideGuess(true);
+		self.alertSuccess(true);
+		self.clearFields();
 	};
 
 	this.modelBindings = ko.observable(Model());
