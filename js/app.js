dbe8104dc1325ab810ae6063a6f240172ec2dbea
Ben Steward <ronyfan102@gmail.com>
2017-07-27 00:57:57 -0400

    Comments!!!
    Documentation was added throughout the entire app (well, not as much in index.html, tbh). Didn't update the ReadMe, which is probably way more important for this particular project, but I want to develop good habits of documenting my code, so that anyone/myself can pick up right where I left off, if that is ever necessary.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 var ViewModel = function() {
 
 	self = this;
-
-	this.modelBindings = ko.observableArray();
-
+	
+	// These observables track the data being edited and displayed in the form and review fields.
 	this.guesserName = ko.observable('');
-	//this.password = ko.observable('');
+		// this.password = ko.observable(''); //Decided against using a password, but kept it in just in case. ;)
 	this.firstName = ko.observable('');
 	this.middleName = ko.observable('');
 	this.gender = ko.observable('');
@@ -14,17 +13,52 @@ var ViewModel = function() {
 	this.length = ko.observable('');
 	this.day = ko.observable('');
 	this.time = ko.observable('');
+
+	/*
+	The genderVal observable is added to the guessObject and stored in
+	firebase by shareGuessToWorld() with the other guess fields, so it can be
+	used to determine the color of the <li> background when the guess is
+	posted to #shared-guesses via the knockout data-bind to css property.
+	*/
 	this.genderVal = ko.observable(true);
-	//this.showHidePassword = ko.observable('password');
+
+	//this.showHidePassword = ko.observable('password'); //Decided against using a password, but kept it in just in case. ;)
+	
+	/*
+	These observables hold boolean values that affect data-bind="css: {'display-none': observableHere}"
+	properties for various DOM elements. These values are altered by various
+	functions to either display (false) or hide (true) these elements.
+	*/
 	this.showHideGuess = ko.observable(true);
-	this.guessObject = {};
 	this.alertNoInput = ko.observable(true);
 	this.alertSuccess = ko.observable(true);
 	this.alertNewEntry = ko.observable(true);
+
+	// Initial object that eventually passes the new guess data to firebase
+	this.guessObject = {};
+
+	/*
+	This observable ensures that the review guesses button only logs one click
+	per page session (assumption: only one user will upload a guess per page load).
+	After submitGuess() is run, the value sets to true, preventing a second click
+	from being logged if the user resubmits the guess before posting it.
+	The rationale here is that I wanted to track the "conversion rate" of
+	reviewed guesses, as I suspected that not every user was completing the process
+	and sharing their guesses to firebase (which is a darn shame).
+	*/
 	this.submittedForReview = ko.observable(false);
+
+	/*
+	This function resets the form, so the user can guess again (which the alert
+	discourages), or another user can step in a submit their guess on the same device.
+	Also, it would be confusing for the form to remain filled out after hitting
+	"share," as it would give the immediate impression that it didn't work,
+	and if the user shared it again, there would be scads of double entries. Not good.
+	*/
 	this.clearFields = function() {
+		// reset the form fields via data-bound observables
 		self.guesserName('');
-		//self.password('');
+		//self.password(''); //Decided against using a password, but kept it in just in case. ;)
 		self.firstName('');
 		self.middleName('');
 		self.gender('');
@@ -33,16 +67,27 @@ var ViewModel = function() {
 		self.length('');
 		self.day('');
 		self.time('');
+		// Boolean switch: results in an alert being displayed that tells the user they
+		// successfully shared their guesses.
 		self.alertNewEntry(false);
 	};
 
+	/* function called when user wants to review their guesses.
+		1. The no input alert is removed, in case it was currently displayed.
+		2. IF they filled in all input fields,
+			a. then their guesses are displayed (using showHideGuess(false)),
+			b. and an alert notifies them of the success (using alertSuccess(false)).
+			c. IF this is the first guess submission of the page load, a click is also logged in firebase.
+		3. IF they left any fields blank,
+			a. an alert reminds them to fill in all the fields.
+			b. success alert is hidden (if it was showing)
+			c. the #guess-summary is hidden, which prevents the user from sharing an incomplete guess.
+	*/
 	this.submitGuess = function() {
 		self.alertNoInput(true);
 		if (self.guesserName() !== '' && self.firstName() !== '' && self.middleName() !== '' && self.lbs() !== '' && self.oz() !== '' && self.length() !== '' && self.day() !== '') {
 			self.alertSuccess(false);
-			if (self.showHideGuess() === true) {
-				self.showHideGuess(false);
-			};
+			self.showHideGuess(false);
 			if (self.submittedForReview() === false) {
 				clickReview();
 				self.submittedForReview(true);
@@ -60,8 +105,19 @@ var ViewModel = function() {
 		} else {
 			self.showHidePassword('password');
 		};
-	};*/
+	};*/ //Decided against using a password, but kept it in just in case. ;)
 
+	/*
+	This function posts the guesses to the database.
+		1. IF
+			a. gender is girl, then genderVal is false.
+			b. gender is boy, then genderVal is true.
+				This boolean is used in the display later on to color the background of the <li>
+		2. The guessObject is constructed.
+		3. The guessObject is passed to firebase via modelToDatabase().
+		4,5,6. The alerts are cleared, the #guess-summary hidden, and form fields reset.
+		7. A click is logged to firebase for the Share button.
+	*/
 	this.shareGuessToWorld = function() {
 		if (self.gender() !== 'Boy') {
 			self.genderVal(false);
@@ -70,7 +126,7 @@ var ViewModel = function() {
 		};
 		self.guessObject = {
 			'guesserName': self.guesserName(),
-			//'password': self.password(),
+			//'password': self.password(), //Decided against using a password, but kept it in just in case. ;)
 			'firstName': self.firstName(),
 			'middleName': self.middleName(),
 			'gender': self.gender(),
@@ -81,23 +137,23 @@ var ViewModel = function() {
 			'time': self.time(),
 			'genderVal': self.genderVal()
 		};
+		modelToDatabase(self.guessObject);
 		self.showHideGuess(true);
 		self.alertSuccess(true);
 		self.clearFields();
-		modelToDatabase(self.guessObject);
 		clickShare();
 	};
 
+	/*
+	Every guessObject that has been passed to firebase is retrieved and this observable
+	updates automatically with each new entry (see database.js for details on the Model),
+	so that the Model data can be bound to the DOM.
+	*/
 	self.modelBindings = ko.computed(function() {
 		return Model()
 	});
 
 };
 
+// Knockout's applyBindings function creates an instance of ViewModel() and, well, applies the bindings to the HTML/DOM.
 ko.applyBindings(new ViewModel());
-
-/* TO DO
-
-push to firebase
-
-*/
--- js/database.js
+++ js/database.js
@@ -1,20 +1,37 @@
+// DATABASE/APP FUNCTIONS
+
+// This observableArray stores the guessObjects by retrieving them from firebase
 var Model = ko.observableArray();
 
+// establishes reference to the location of the guessObjects
 var guesses = firebase.database().ref('guesses');
 
+/*
+an event listener built into firebase that sends any added children
+to the Model via JSON format.
+*/
 guesses.on('child_added', function(data) {
 	Model.push(data.val());
 });
 
+/*
+This function takes a parameter that is then translated into a new
+guesses node. In app.js, shareGuessToWorld() passes the guessObject into
+this function.
+*/
 var modelToDatabase = function(data) {
 	newGuess = guesses.push();
 	newGuess.set(data);
 }
 
-// Click Data
+// CLICK DATA FUNCTIONS
 
+// establishes a reference for storing clicks of the review button
 var reviewClicks = firebase.database().ref('stats/reviewClicks');
 
+// ***** Read about the transaction method in firebase here: https://firebase.google.com/docs/reference/js/firebase.database.Reference#transaction
+
+// when called, this function increments the reviewClick count by 1.
 var clickReview = function() {
   reviewClicks.transaction(function(clicks) {
   	if (clicks === null) {
@@ -24,8 +41,10 @@ var clickReview = function() {
   });
 }
 
+// establishes a reference for storing clicks of the share button
 var shareClicks = firebase.database().ref('stats/shareClicks');
 
+// when called, this function increments the shareClick count by 1.
 var clickShare = function() {
   shareClicks.transaction(function(clicks) {
   	if (clicks === null) {
@@ -34,20 +53,3 @@ var clickShare = function() {
   	return clicks + 1;
   });
 }
-/*function nameOfFunc(postRef, uid) {
-  postRef.transaction(function(post) {
-    if (post) {
-      if (post.stars && post.stars[uid]) {
-        post.starCount--;
-        post.stars[uid] = null;
-      } else {
-        post.starCount++;
-        if (!post.stars) {
-          post.stars = {};
-        }
-        post.stars[uid] = true;
-      }
-    }
-    return post;
-  });
-}*/
